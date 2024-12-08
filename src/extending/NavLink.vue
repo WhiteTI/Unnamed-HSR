bdaaff0a0e746e79640c9e60954c737146fb6fa4
white <grigsgt@gmail.com>
2024-12-08 21:28:08 +0300

    extends RouterLink, add active style

---
+++ src/extending/NavLink.vue
@@ -0,0 +1,20 @@
+<script setup>
+import {RouterLink} from "vue-router";
+
+defineOptions({
+    inheritAttrs: false
+})
+
+const props = defineProps({
+    ...RouterLink.props,
+    activeStyle: Object
+})
+</script>
+
+<template>
+    <RouterLink v-bind="$props" custom v-slot="{isActive, href, navigate}">
+        <a v-bind="$attrs" :href="href" @click="navigate" :style="isActive ? activeStyle : null">
+            <slot/>
+        </a>
+    </RouterLink>
+</template>
