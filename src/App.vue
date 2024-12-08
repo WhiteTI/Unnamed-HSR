17561ae78b026e999a2184f7c40106eac9da8ac0
white <grigsgt@gmail.com>
2024-12-08 21:28:49 +0300

    app

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,14 @@
 <script setup>
-
+import Sidebar from "@/components/sidebar/Sidebar.vue";
+import Footer from "@/components/footerComponent/FooterComponent.vue";
 </script>
 
 <template>
-
+    <Sidebar/>
+    <div style="padding-left: 64px">
+        <RouterView/>
+        <Footer/>
+    </div>
 </template>
 
 <style scoped>
